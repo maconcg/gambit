083972cc7c28ceb036b80432c87039a571093c85
Macon Gambill <macon@arstne.io>
2024-10-08 12:54:40 -0400

    doc2e: update .dir-locals.el for use with "Makefile"

--- doc/2e/.dir-locals.el
+++ doc/2e/.dir-locals.el
@@ -1,4 +1,4 @@
-((auto-mode-alist . (("/makefile\\'" . makefile-mode)))
+((auto-mode-alist . (("/Makefile\\'" . makefile-mode)))
  (nil . ((indent-tabs-mode . nil)))
  (makefile-mode . ((fill-column . 79)))
  (texinfo-mode . ((fill-column . 72))))
