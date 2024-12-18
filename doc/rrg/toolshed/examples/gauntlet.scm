990a45931affd1e07979c59729ebc92a8d76088a
Macon Gambill <macon@arstne.io>
2024-12-18 09:54:50 -0500

    rectangular notation for complex numbers should now work

--- doc/rrg/toolshed/examples/gauntlet.scm
+++ doc/rrg/toolshed/examples/gauntlet.scm
@@ -28,7 +28,7 @@
 #i#b1 #O#e7 #e#O6 #X#Ee #e#xe #x#if #x#ICE #e#d10e0 #e+3 #I-9 #d#i-4 #D#I+9l1 4
 44 .4 .4s4 -.3 +.4 #d#I+.8 #e#X+F 2. #d2. #e#d2. #d#E2. #D.8 #e.8s2 #e9. #D#E.0
 #x#i-InF.0 #i#O+nAN.0 #i#b+inf.0 #x#if #E#xe #e+.4 #e+.4e41 #e#d+.4e4 #D#i-.4s4
--2.5+0.0i 3+0i +inf.0 3.0+inf.0i 32/5
+-2.5+0.0i 3+i +inf.0 3.0+inf.0i 32/5 +nan.0+5.0i +inf.0-inf.0i +NAn.0-inf.0i +i
 ;; Not numbers:
 #b#i12 #b21 #o87 #da1 #xg2 #e#d10ea #o7e1 + - ++ -- +- -+ + 1+ 1- +1+ -1- 12e6.
 +nan.1 +inf.00 -inf.0e1 #e+ #I+ .. .4. ..2 #D#E. #b#o0 #e+inf.0 #E+Nan.0 #e#x#e
